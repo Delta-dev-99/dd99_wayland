abbbcf90aa3cd0eda9ae5f9c2231dfd69498bac3
dd99 <delta.deltadev.127@gmail.com>
2024-05-25 20:11:37 -0400

    some work on the wayland engine

--- dd99_wayland/include/dd99/wayland/engine.hpp
+++ dd99_wayland/include/dd99/wayland/engine.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <bits/types/struct_iovec.h>
+#include <sys/types.h>
+
 #include <concepts>
 #include <memory>
 #include <span>
+#include <cstdint>
+#include <utility>
 
 
 
 namespace dd99::wayland
 {
+    namespace proto{ struct interface; }
+
     namespace detail
     {
         struct engine_impl;
     }
+
 
 
     // signature for the output callback used by the engine
@@ -20,6 +28,8 @@ namespace dd99::wayland
 
     // engine template declaration
     template <class OutCB = engine_cb_t> struct engine;
+
+
 
 
 
@@ -32,12 +42,19 @@ namespace dd99::wayland
     template<>
     struct engine<engine_cb_t>
     {
+    public:
+        using version_t = std::uint32_t;
+        using object_id_t = std::uint32_t;
+        using opcode_t = std::uint16_t;
+        using message_length_t = std::uint16_t;
+
+
     public: // constructors
 
         // the user MUST set the callback to a valid function pointer
         // before calling any i/o member function.
         // output_cb: callback called when engine wants to output something
-        engine(engine_cb_t output_cb = nullptr);
+        engine(engine_cb_t output_cb = nullptr, void * user_data = nullptr);
 
 
         engine(const engine &) = delete; // no copy
@@ -46,13 +63,18 @@ namespace dd99::wayland
     
     public: // I/O API
     
-        // The user is expected to manually read data from the wayland server connection.
+        // The user is expected to manually read data from the wayland server connection and handle buffering.
         // When data arrives from the server, use this function to give it to the display.
         // This function dispatches the received events to the apropiate callbacks.
         // 
-        // TODO: consider partial data frames. This requires buffering.
-        // TODO: consider returning some state code.
-        void put_input(std::span<unsigned char> data);
+        // About buffering:
+        // The user is expected to buffer received data. This handles the possibility
+        // of partial messages that must be kept until they are processed.
+        // The return value can be used to indicate whether there is a partial message in buffer.
+        // This function takes `iovec` (scatter/gather style) input to allow efficient implementations using ringbuffers.
+        // 
+        // @RETURN: bytes consumed
+        std::size_t process_input(iovec * data, std::size_t iovec_count);
 
         // The user is expected to manually write data to the wayland server connection.
         // When there is data to be sent to the wayland server, this callback gets called.
@@ -72,6 +94,21 @@ namespace dd99::wayland
         // int get_error();
         // uint32_t get_protocol_error();
 
+    
+    protected: // API exposed to interfaces
+        friend struct dd99::wayland::proto::interface;
+
+        template <class Interface> std::pair<object_id_t, Interface> create_interface();
+        void destroy_iterface(const proto::interface & x); // called by interface destructors
+
+        struct engine_accessor
+        {
+            engine & m_engine;
+            template <class Interface> auto create_interface() { return m_engine.create_interface<Interface>(); }
+
+            void destroy_interface(const proto::interface & x) { return m_engine.destroy_iterface(x); }
+        };
+
 
     private:
         using impl_t = detail::engine_impl;
@@ -80,6 +117,8 @@ namespace dd99::wayland
         
         std::unique_ptr<impl_t, impl_deleter_t> impl;
     };
+
+
 
 
 
@@ -102,6 +141,8 @@ namespace dd99::wayland
     private:
         OutCB output_cb;
     };
+
+
 
 
 
--- dd99_wayland/src/engine.cpp
+++ dd99_wayland/src/engine.cpp
@@ -1,33 +1,145 @@
 #include <dd99/wayland/engine.hpp>
+#include <dd99/wayland/interface.hpp>
+#include "object_map.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <iterator>
+#include <memory>
+#include <numeric>
+#include <vector>
 
 
 
 namespace dd99::wayland
 {
 
+    // struct wayland_object
+    // {
+    //     std::uint32_t object_id;
+    // };
+
     namespace detail
     {
 
+        // this is the actual engine implementation
         struct engine_impl
         {
-            engine_cb_t cb;
-            void * user_data;
+            using object_id_t = engine<>::object_id_t;
+            using message_size_t = engine<>::message_length_t;
+            
+            static constexpr object_id_t client_object_id_base = 1;
+            static constexpr object_id_t server_object_id_base = 0xf0000000;
+
+
+            engine_impl(engine_cb_t _cb, void * _user_data)
+                : cb{_cb}
+                , user_data{_user_data}
+            { }
+
+
+
+            std::size_t process_input(iovec * data, std::size_t iovec_count);
+
+
+
+            engine_cb_t cb; // output callback
+            void * user_data; // user data for output callback
+            
+            object_map<proto::interface, object_id_t, client_object_id_base> m_client_object_map{};
+            object_map<proto::interface, object_id_t, server_object_id_base> m_server_object_map{};
         };
 
     }
 
 
 
-    engine<engine_cb_t>::engine(engine_cb_t cb)
-        : impl(new detail::engine_impl{cb}, [](detail::engine_impl * p){delete p;})
-    {
 
-    }
+
+    engine<engine_cb_t>::engine(engine_cb_t cb, void * user_data)
+        : impl(new detail::engine_impl{cb, user_data}, [](detail::engine_impl * p){delete p;})
+    { }
 
     void engine<engine_cb_t>::set_output_callback(engine_cb_t cb, void * user_data)
     {
         impl->cb = cb;
         impl->user_data = user_data;
+    }
+
+    std::size_t engine<engine_cb_t>::process_input(iovec * data, std::size_t iovec_count)
+    {
+        return impl->process_input(data, iovec_count);
+    }
+
+
+
+
+
+    namespace detail
+    {
+        std::size_t engine_impl::process_input(iovec * data, std::size_t iovec_count)
+        {
+            if (iovec_count == 0) return 0;
+
+            const std::size_t total_data = std::accumulate(data, data + iovec_count, std::size_t{}, 
+                [](std::size_t && acc, iovec & v){ return std::move(acc) + v.iov_len; });
+
+            std::size_t consumed = 0;
+            // std::size_t current_buf = 0;
+            // std::size_t buf_pos = 0;
+
+            auto extract_data = [&]<class T>() -> const T &
+            {
+                const auto available_contiguous_data = data[current_buf].iov_len - buf_pos;
+                const auto data_ptr = static_cast<const char *>(data[current_buf].iov_base) + buf_pos;
+                // check data element is contiguous in buffer and meets alignment requirements
+                if ((available_contiguous_data >= sizeof(T)) &&
+                    (reinterpret_cast<std::uintptr_t>(data_ptr) & (alignof(T) - 1) == 0))
+                {
+                    return *static_cast<const T *>(data_ptr);
+                }
+                else
+                {
+                    alignas(T) char elem_data[sizeof(T)];
+                    auto remaining = sizeof(T);
+                    std::copy_n(data_ptr, std::min(data[current_buf].iov_len - buf_pos, remaining), elem_data);
+                }
+            };
+
+            // // *** TODO: do this the right way
+            // static std::vector<char> buffer;
+            // std::size_t sz = 0;
+            // for (std::size_t i = 0; i < iovec_count; ++i) sz += data[i].iov_len;
+            // buffer.clear();
+            // buffer.reserve(sz);
+            // if (buffer.capacity() < sz) buffer.resize(sz); // just to be sure
+            // for (std::size_t i = 0; i < iovec_count; ++i)
+            //     std::copy(reinterpret_cast<char *>(data[i].iov_base)
+            //             , reinterpret_cast<char *>(data[i].iov_base) + data[i].iov_len
+            //             , std::back_inserter(buffer));
+
+            for (;;)
+            {
+                const auto available_data = total_data - consumed;
+
+                constexpr auto hdr_size = sizeof(object_id_t) + sizeof(message_size_t);
+                if (available_data < hdr_size) break;
+
+                const object_id_t & msg_obj_id = * reinterpret_cast<object_id_t *>(buffer.data() + consumed);
+                message_size_t & msg_size = * reinterpret_cast<message_size_t *>(buffer.data() + consumed + sizeof(msg_obj_id));
+
+                if (available_data < msg_size) break;
+
+                consumed += msg_size;
+
+                // auto x = m_client_object_map[msg_obj_id]->parse_and_dispatch_event()
+                // get_object().parse_and_dispatch_event(buffer.data() + consumed);
+            }
+
+            return consumed;
+        }
     }
 
 }
