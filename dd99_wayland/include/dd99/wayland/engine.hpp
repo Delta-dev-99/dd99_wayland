871fe34461c4d4397633cf42d34824d82a066def
dd99 <delta.deltadev.127@gmail.com>
2024-05-30 00:33:48 -0400

    new engine architecture using virtual functions Unified engine interface. No longer required accessor (maybe later)

--- dd99_wayland/include/dd99/wayland/engine.hpp
+++ dd99_wayland/include/dd99/wayland/engine.hpp
@@ -1,73 +1,65 @@
 #pragma once
 
+
+#include <dd99/wayland/types.hpp>
+
+#include <cassert>
 #include <concepts>
+#include <cstddef>
 #include <memory>
 #include <span>
-#include <cstdint>
-#include <utility>
 
 
 
 namespace dd99::wayland
 {
-    namespace proto{
-        struct interface;
-        namespace wayland{
-            struct display;
-        }
-    }
 
-    namespace detail { struct engine_impl; }
+    // fw-declaration of wayland display interface
+    // namespace proto{
+    //     struct interface;
+    //     namespace wayland{
+    //         struct display;
+    //     }
+    // }
 
 
+    // fw-declarations
+    namespace proto { struct interface; }
+    namespace proto::wayland { struct display; }
 
-    // signature for the output callback used by the engine
-    using engine_cb_signature = void(void * user_data, std::span<unsigned char> output_data, std::span<int> ancillary_output_fd_collection);
-    using engine_cb_t = engine_cb_signature *;
-
-    // engine template declaration
-    template <class OutCB = engine_cb_t> struct engine;
-
+    // for pimpl
+    namespace detail { struct engine_data; }
 
 
 
 
-    // Core of wayland interaction
-    // This class acts as an engine for (de)marshaling and dispatching of wayland protocol data
-    // 
-    // This is the default template specialization
-    // Other template specializations are wrappers around this one
-    // This template specialization implements the engine logic
-    template<>
-    struct engine<engine_cb_t>
+    
+    // The reading/writing of data through the connection to wayland server is up to the user
+    // To use: inherit from this class and define the virtual methods
+    struct engine
     {
+    protected:
+        // virtual destruction not allowed
+        // you must destruct the engine instance through a properly typed instance object destructor
+        ~engine() = default;
+
+
     public:
-        using version_t = std::uint32_t;
-        using object_id_t = std::uint32_t;
-        using opcode_t = std::uint16_t;
-        using message_length_t = std::uint16_t;
-
-
-    public: // constructors
-
-        // the user MUST set the callback to a valid function pointer
-        // before calling any i/o member function.
-        // output_cb: callback called when engine wants to output something
-        engine(engine_cb_t output_cb = nullptr, void * user_data = nullptr);
-
+        engine();
 
         engine(const engine &) = delete; // no copy
         engine(engine&&) = default; // default move
 
     
-    public:
-        // get a reference of the root object of the protocol: the wayland display
-        // dd99::wayland::proto::wayland::display & get_display();
-        void bind_display();
+    public: // Wayland-related API
 
-    
+        // create display of user-derived type
+        template <std::derived_from<proto::wayland::display> T = proto::wayland::display, class ... Args>
+        T & create_display(Args && ... args);
+
+
     public: // I/O API
-    
+
         // The user is expected to manually read data from the wayland server connection and handle buffering.
         // When data arrives from the server, use this function to give it to the display.
         // This function dispatches the received events to the apropiate callbacks.
@@ -82,89 +74,61 @@ namespace dd99::wayland
         // std::size_t process_input(iovec * data, std::size_t iovec_count);
         std::size_t process_input(std::span<char> data);
 
-        // The user is expected to manually write data to the wayland server connection.
+
+    public: // I/O Events that MUST be implemented by derived clases
+        
         // When there is data to be sent to the wayland server, this callback gets called.
         // Calling this function is always the result of interacting with wayland objects in this library.
         // 
         // Signature:
         //  `data` is just binary data to be sent to the server
         //  `fds` is a collection of file descriptors to be sent as ancillary data
-        // 
-        // About buffering:
-        // This library does not provide output buffering.
-        // The user may choose to buffer this data before sending to the wayland server.
-        // Note, however, that the server might expect timely responses to some events.
-        void set_output_callback(engine_cb_t cb, void* user_data);
+        virtual void on_output(std::span<char> data, std::span<int> ancillary_output_fd_collection) = 0;
 
 
-        // int get_error();
-        // uint32_t get_protocol_error();
+    public: // internal functions used by protocol interfaces
 
-    
-    protected: // API exposed to interfaces
-        friend struct dd99::wayland::proto::interface;
+        std::pair<object_id_t, proto::interface &> bind_interface(std::unique_ptr<proto::interface>);
 
-        template <class Interface> std::pair<object_id_t, Interface &> create_interface(); // called by interfaces on messages
-        void destroy_iterface(const proto::interface & x); // called by interface destructors
+        template <class T, class ... Args>
+        std::pair<object_id_t, T &> allocate_interface(Args && ... args);
 
-        // accessor for protocol-defined interfaces
-        struct engine_accessor
-        {
-            engine & m_engine;
-            template <class Interface> auto create_interface() { return m_engine.create_interface<Interface>(); }
-
-            void destroy_interface(const proto::interface & x) { return m_engine.destroy_iterface(x); }
-        };
-
-    
-    private:
-        std::pair<object_id_t, dd99::wayland::proto::interface &> store_new_interface(std::unique_ptr<dd99::wayland::proto::interface>);
+        void free_interface(proto::interface &);
 
 
-    private:
-        using impl_t = detail::engine_impl;
-        using impl_deleter_signature = void(impl_t*);
-        using impl_deleter_t = impl_deleter_signature *;
+    private: // auxiliary type definitions
+
+        // obscure data type used for dependency decoupling
+        using data_t = detail::engine_data;
+
+        // implementation deleter type (just a function ptr)
+        // used for decoupling `impl_t`'s destructor implementation from the API
+        using data_deleter_signature = void(data_t*);
+        using data_deleter_t = data_deleter_signature *;
+
+
+    private: // data members
         
-        std::unique_ptr<impl_t, impl_deleter_t> impl;
+        std::unique_ptr<data_t, data_deleter_t> m_data_ptr;
+
     };
 
-    template <class Interface>
-    inline auto engine<engine_cb_t>::create_interface() -> std::pair<object_id_t, Interface &>
+
+
+    template<class T, class ... Args>
+    std::pair<object_id_t, T &> engine::allocate_interface(Args && ... args)
     {
-        // Interface x{.m_engine = engine_accessor{*this}};
-        auto && [id, new_interface_ref] = store_new_interface(std::make_unique<Interface>(engine_accessor{*this}));
-        return {id, *reinterpret_cast<Interface *>(& new_interface_ref)};
+        auto && [new_id, new_interface] = bind_interface(new T{*this, std::forward<Args>(args)...});
+        return {new_id, reinterpret_cast<T&>(new_interface)};
     }
 
 
-
-
-
-    // wrapper to allow custom callback types
-    // this is the main definition of the template
-    template <class OutCB>
-    struct engine : engine<engine_cb_t>
+    template <std::derived_from<proto::wayland::display> T, class ... Args>
+    T & engine::create_display(Args && ... args)
     {
-        static_assert(std::is_invocable_v<OutCB, std::span<unsigned char>, std::span<int>>, "OutCB signature is incorrect!");
-        using base = engine<engine_cb_t>;
-
-        // create and set callback. Useful for use with lambdas
-        engine(OutCB && cb)
-            : base{[](void * instance, std::span<unsigned char> data, std::span<int> fd){ reinterpret_cast<engine *>(instance)->output_cb(data, fd); }}
-            , output_cb{std::move(cb)}
-        { }
-
-        void set_output_callback(OutCB && cb) { output_cb = std::forward<OutCB>(cb); }
-
-
-    private:
-        OutCB output_cb;
-    };
-
-    // engine template deduction guide to prioritize basic callback form
-    template <std::convertible_to<engine_cb_t> T>
-    engine(T&&) -> engine<engine_cb_t>;
-    
+        auto && [new_id, new_interface] = bind_interface(std::make_unique<T>(*this, std::forward<Args>(args)...));
+        assert(new_id == 1);
+        return reinterpret_cast<T&>(new_interface);
+    }
 
 }
--- dd99_wayland/include/dd99/wayland/interface.hpp
+++ dd99_wayland/include/dd99/wayland/interface.hpp
@@ -1,106 +1,106 @@
 #pragma once
 
 
-#include <bits/types/struct_iovec.h>
-#include <concepts>
-#include <cstdint>
+#include <dd99/wayland/types.hpp>
 #include <dd99/wayland/config.hpp>
 #include <dd99/wayland/engine.hpp>
+
+// #include <bits/types/struct_iovec.h>
+
+#include <concepts>
+#include <cstdint>
 #include <format>
 #include <source_location>
 #include <stdexcept>
 
 
-namespace dd99::wayland
+
+namespace dd99::wayland::proto
 {
-    namespace proto
+
+    // fw declaration
+    struct interface;
+
+    template <class T, class Id_T, Id_T Base>
+    struct object_map;
+
+
+    // concept of interface: derives from dd99::wayland::proto::interface
+    template <class T> concept Interface_C = std::derived_from<T, interface>;
+
+
+
+    // base class for all protocol-defined interfaces
+    // objects are instances of derived classes
+    struct interface
     {
+    protected: // types
+        friend dd99::wayland::engine;
+        // using engine_t = engine<>;
+        // // friend engine_t;
+        // friend dd99::wayland::detail::engine_impl;
+        // using object_id_t = engine_t::object_id_t;
+        // using version_t = engine_t::version_t;
+        // using opcode_t = engine_t::opcode_t;
 
-        struct fixed_point
+
+    protected: // member variables
+        // engine_t::engine_accessor m_engine;
+        engine & m_engine;
+        std::uint32_t m_object_id = 0;
+
+
+    protected:
+
+        interface(engine & eng)
+            : m_engine{eng}
+        { }
+
+
+    public: // destructor
+
+        virtual ~interface() = default;
+    
+    
+    protected:
+        // static data annexed to each interface class
+        struct static_data_t
         {
-            constexpr explicit fixed_point(double v) : m_value{static_cast<std::int32_t>(v * 256.0)} {}
-            constexpr explicit operator double() { return to_double(); }
-            constexpr double to_double() { return m_value / 256.0; }
-
-        private:
-            std::int32_t m_value;
+            version_t server_supported_version = 0;
         };
 
-        // base class for all protocol-defined interfaces
-        // objects are instances of derived classes
-        struct interface
+    
+    protected: // functions exposed to derived classes
+        template <class ... Args>
+        void send_wayland_message(opcode_t opcode, Args && ... args);
+
+
+    protected: // functions that derived classes must implement
+        virtual void parse_and_dispatch_event(std::span<char> data) = 0;
+
+
+    protected: // for debugging
+        constexpr void dbg_check_version(version_t minimum_required_version,
+                                            version_t server_supported_version,
+                                            std::string_view interface_name,
+                                            const std::source_location& location = std::source_location::current()) 
         {
-        protected: // types
-            using engine_t = engine<>;
-            // friend engine_t;
-            friend dd99::wayland::detail::engine_impl;
-            using object_id_t = engine_t::object_id_t;
-            using version_t = engine_t::version_t;
-            using opcode_t = engine_t::opcode_t;
-
-
-        protected: // member variables
-            engine_t::engine_accessor m_engine;
-            std::uint32_t m_object_id = 0;
-
-
-        public: // destruction
-            virtual ~interface() { m_engine.destroy_interface(*this); };
-
-
-        protected: // construction (only by engine)
-            interface(engine_t::engine_accessor engine_accessor)
-                : m_engine{engine_accessor}
-            { }
-
-        
-        // public:
-        //     auto get_id() const { return m_object_id; }
-
-
-        protected:
-            // static data annexed to each interface
-            struct static_data_t
+            if constexpr (is_debug_enabled())
             {
-                version_t server_supported_version = 0;
-            };
-
-        
-        protected: // functions exposed to derived classes
-            template <class ... Args>
-            void send_wayland_message(opcode_t opcode, Args && ... args);
-
-
-        protected:
-            virtual void parse_and_dispatch_event(std::span<char> data) = 0;
-
-
-        protected: // for debugging
-            constexpr void dbg_check_version(version_t minimum_required_version,
-                                             version_t server_supported_version,
-                                             std::string_view interface_name,
-                                             const std::source_location& location = std::source_location::current()) 
-            {
-                if constexpr (is_debug_enabled())
-                {
-                    if (server_supported_version < minimum_required_version)
-                        throw std::runtime_error{std::format(
-                                "[DD99_WAYLAND] In file {}:{}:{}: In function {}: ERROR: Version check failed. Server supported version: {}. Minimum required version: {}."
-                            , location.file_name()
-                            , location.line()
-                            , location.column()
-                            , location.function_name()
-                            , server_supported_version
-                            , minimum_required_version
-                            , interface_name)};
-                }
+                if (server_supported_version < minimum_required_version)
+                    throw std::runtime_error{std::format(
+                            "[DD99_WAYLAND] In file {}:{}:{}: In function {}: ERROR: Version check failed. Server supported version: {}. Minimum required version: {}."
+                        , location.file_name()
+                        , location.line()
+                        , location.column()
+                        , location.function_name()
+                        , server_supported_version
+                        , minimum_required_version
+                        , interface_name)};
             }
-        };
+        }
+    };
 
-
-        template <class T> concept Interface_C = std::derived_from<T, interface>;
-
-    }
 }
 
 
