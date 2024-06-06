8d05f1c5da66b10414805ab6afdce5dfd8495ab9
dd99 <delta.deltadev.127@gmail.com>
2024-06-06 02:47:25 -0400

    updated test code

--- test_sources/test1.cpp
+++ test_sources/test1.cpp
@@ -1,18 +1,40 @@
+#include "asio/buffer.hpp"
+#include "asio/buffered_write_stream.hpp"
 #include "dd99-wayland-client-protocol-wayland.hpp"
+#include "dd99/wayland/engine.hpp"
+#include <cstring>
 #include <dd99/wayland/wayland_client.hpp>
 
 #include <asio.hpp>
 
+#include <format>
+#include <iostream>
+#include <iterator>
 #include <stdexcept>
+#include <sys/socket.h>
 #include <thread>
 #include <vector>
-
 #include <chrono>
 #include <cstdlib>
 #include <filesystem>
 
 
 namespace wlp = dd99::wayland::proto;
+
+
+
+// struct wayland_app
+// {
+//     static wayland_app & get_instance() { static wayland_app instance; return instance; }
+
+// private:
+//     wayland_app() = default;
+
+
+// private:
+
+// };
+
 
 
 
@@ -41,47 +63,78 @@ auto connect_to_wayland_server(asio::io_context & io_ctx)
 }
 
 
-struct engine : dd99::wayland::engine
+struct engine final : dd99::wayland::engine
 {
-    engine(asio::local::stream_protocol::socket & sock)
+    using sock_type = asio::buffered_write_stream<asio::local::stream_protocol::socket &>;
+
+    engine(sock_type & sock)
         : m_sock(sock)
     {}
 
 
-    void on_output(std::span<char> data, std::span<int> ancillary_output_fd_collection) override
+    void on_output(std::span<const char> data, std::span<int> ancillary_output_fd_collection) override
     {
         asio::write(m_sock, asio::buffer(data));
     }
 
 
-    asio::local::stream_protocol::socket & m_sock;
+    sock_type & m_sock;
+};
+
+
+// struct display final : dd99::wayland::proto::wayland::display
+// {
+    
+// };
+
+struct registry : dd99::wayland::proto::wayland::registry
+{
+    using dd99::wayland::proto::wayland::registry::registry;
+
+    void on_global(std::uint32_t name, std::string_view interface, std::uint32_t version) override
+    {
+        std::format_to(std::ostream_iterator<char>(std::cout), 
+            "registry::on_global    name: {:2}    version: {:2}    interface: {}\n"
+        , name, version, interface);
+    }
 };
 
 
 
 int main()
 {
+    // auto newsock = socket(PF_LOCAL, SOCK_STREAM | SOCK_CLOEXEC, 0);
+
     asio::io_context io_ctx;
 
     auto sock = connect_to_wayland_server(io_ctx);
 
+    asio::buffered_write_stream<decltype(sock) &> bufsock{sock, 1024*4};
+
     // connected!
 
-    engine wl_eng{sock};
+    engine wl_eng{bufsock};
     auto & display = wl_eng.create_display();
+    auto & reg = display.get_registry<registry>();
+
     // auto && [display_id, wl_display] = wl_eng.allocate_interface<dd99::wayland::proto::wayland::display>();
     // auto disp = wl_eng.get_display();
     // wlp::wayland::display disp{}
 
 
+    asio::error_code ec;
 
 
     std::vector<char> buf;
-    buf.reserve(2048);
+    buf.resize(1024*4);
+    std::size_t chars_in_buf = 0;
     for (;;) {
-        auto n [[maybe_unused]] = sock.read_some(asio::buffer(buf));
-        auto n_consumed = wl_eng.process_input(buf);
-        buf.erase(buf.begin(), buf.begin() + n_consumed);
-        std::this_thread::sleep_for(std::chrono::microseconds{20});
+        auto n_written [[maybe_unused]] = bufsock.flush();
+        chars_in_buf += bufsock.read_some(asio::buffer(buf.data() + chars_in_buf, buf.size() - chars_in_buf), ec);
+        auto n_consumed = wl_eng.process_input({buf.data(), buf.data() + chars_in_buf});
+        if (n_consumed != chars_in_buf) std::memmove(buf.data(), buf.data() + n_consumed, chars_in_buf - n_consumed);
+        chars_in_buf -= n_consumed;
+        // buf.erase(buf.begin(), buf.begin() + n_consumed);
+        // std::this_thread::sleep_for(std::chrono::microseconds{20});
     }
 }
