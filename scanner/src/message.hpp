71c94d9ed617b4756c147314b60245d87222d8aa
dd99 <delta.deltadev.127@gmail.com>
2024-06-08 02:11:37 -0400

    updated function code and prototype generation to match current structure fixed a bug where wire argument ordering was messed up

--- scanner/src/message.hpp
+++ scanner/src/message.hpp
@@ -14,7 +14,7 @@ struct message_t : element_t
     int since;
     int opcode;
     std::vector<argument_t> args{};
-    std::size_t ret_index = std::numeric_limits<std::size_t>::max();
+    // std::size_t ret_index = std::numeric_limits<std::size_t>::max();
 
     // message_t(const message_t &) = delete;
     // void operator=(const message_t &) = delete;
@@ -27,8 +27,8 @@ struct message_t : element_t
         for (const auto arg_node : node.children("arg"))
         {
             auto & i = args.emplace_back(arg_node);
-            if (i.base_type.type == argument_type_t::type_t::TYPE_NEWID)
-                ret_index = args.size() - 1;
+            // if (i.base_type.type == argument_type_t::type_t::TYPE_NEWID)
+            //     ret_index = args.size() - 1;
         }
     }
 
@@ -84,7 +84,7 @@ struct message_t : element_t
     // outgoing
     void print_definition(code_generation_context_t & ctx, std::string_view scope = {}) const
     {
-        auto has_return_type = ret_index != std::numeric_limits<std::size_t>::max();
+        // auto has_return_type = ret_index != std::numeric_limits<std::size_t>::max();
         // auto returns_unknown_interface = has_return_type && (args[ret_index].base_type.type == argument_type_t::type_t::TYPE_NEWID) && args[ret_index].interface.empty();
 
 
@@ -98,46 +98,45 @@ struct message_t : element_t
             "{0}{{\n"
             "{1}constexpr version_t since = {2};\n"
             "{1}constexpr opcode_t opcode = {3};\n"
+            "\n"
+            "{1}assert(m_object_id != 0);\n"
+            "{1}dbg_check_version(since, static_data.server_supported_version, interface_name);\n"
+            "\n"
         , whitespace{ctx.indent_size * ctx.indent_level}
         , whitespace{ctx.indent_size * (ctx.indent_level + 1)}
         , since
         , opcode);
-        if (has_return_type) 
+
+        for (const auto & arg : args)
         {
-            ctx.output.format("\n{}", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
-            // if (returns_unknown_interface)
-            // {
-                ctx.output.write("auto [new_id, new_interface] = m_engine.allocate_interface<T>();\n");
-            // }
-            // else
-            // {
-            //     ctx.output.write("auto [new_id, new_interface] = m_engine.allocate_interface<");
-            //     args[ret_index].print_type(ctx);
-            //     ctx.output.write(">();\n");
-            // }
-            // ctx.output.format("{}const auto new_id = new_interface.get_id();\n", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
+            if (arg.base_type.type != argument_type_t::type_t::TYPE_NEWID) continue;
+
+            ctx.output.format("{}", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
+            ctx.output.write("auto new_");
+            arg.print_name(ctx);
+            ctx.output.write(" = m_engine.bind_interface(");
+            arg.print_name(ctx);
+            ctx.output.write(");\n");
         }
-        ctx.output.format(""
-            "\n"
-            "{0}dbg_check_version(since, static_data.server_supported_version, interface_name);\n"
-            "\n"
-            "{0}send_wayland_message(opcode, {{}}"
-        , whitespace{ctx.indent_size * (ctx.indent_level + 1)});
-        if (has_return_type) ctx.output.write(", new_id");
+        // if (has_return_type)
+        // {
+        //     ctx.output.format("{}", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
+        //     ctx.output.write("auto new_id = m_engine.bind_interface(");
+        //     args[ret_index].print_name(ctx);
+        //     ctx.output.write(");\n\n");
+        // }
+        ctx.output.format("{}", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
+        ctx.output.write("send_wayland_message(opcode, {}");
+        // if (has_return_type) ctx.output.write(", new_id");
         // passing arguments to `send_wayland_message`
         for (const auto & arg : args)
         {
-            if (arg.base_type.type == argument_type_t::type_t::TYPE_NEWID) continue;
             ctx.output.write(", ");
+            if (arg.base_type.type == argument_type_t::type_t::TYPE_NEWID) ctx.output.write("new_");
             arg.print_name(ctx);
         }
         // end of `send_wayland_message` invocation
         ctx.output.write(");\n");
-        // returning 
-        if (has_return_type)
-        {
-            ctx.output.format("{}return new_interface;\n", whitespace{ctx.indent_size * (ctx.indent_level + 1)});
-        }
         // closing brace
         ctx.output.format(""
             "{0}}}\n"
@@ -148,45 +147,41 @@ private:
     // indentation, return type, function name and arguments (no semicolon)
     void print_prototype(code_generation_context_t & ctx, std::string_view scope = {}) const
     {
-        auto has_return_type = ret_index != std::numeric_limits<std::size_t>::max();
-        auto returns_interface = has_return_type && (args[ret_index].base_type.type == argument_type_t::type_t::TYPE_NEWID);
-        auto returns_unknown_interface = returns_interface && args[ret_index].interface.empty();
+        // auto has_return_type = ret_index != std::numeric_limits<std::size_t>::max();
+        // auto returns_interface = has_return_type && (args[ret_index].base_type.type == argument_type_t::type_t::TYPE_NEWID);
+        // auto returns_unknown_interface = returns_interface && args[ret_index].interface.empty();
 
         // indentation
         ctx.output.format("{}", whitespace{ctx.indent_size * ctx.indent_level});
 
         // make template in this case
-        if (returns_unknown_interface)
-            ctx.output.write("template <Interface_C T> ");
-        else if (returns_interface)
-            ctx.output.format("template <std::derived_from<{}> T> ", remove_wayland_prefix(args[ret_index].interface));
+        // if (returns_unknown_interface)
+        //     ctx.output.write("template <Interface_C T> ");
+        // else if (returns_interface)
+        //     ctx.output.format("template <std::derived_from<{}> T> ", remove_wayland_prefix(args[ret_index].interface));
 
         // inline
         if (!scope.empty()) ctx.output.write("inline ");
 
-        //return type
-        if (returns_interface) ctx.output.write("T &");
-        else if (has_return_type) args[ret_index].print_type(ctx);
-        else ctx.output.write("void");
+        // return type
+        ctx.output.write("void ");
 
         // function name
-        if (!scope.empty()) ctx.output.format(" {}::", scope);
-        else ctx.output.put(' ');
+        if (!scope.empty()) ctx.output.format("{}::", scope);
         ctx.output.write(name);
         ctx.output.put('(');
-
 
         // function arguments
         bool is_first_arg = true;
         for (const auto & argument : args)
         {
-            if (argument.base_type.type == argument_type_t::type_t::TYPE_NEWID)
-                continue;
+            // if (argument.base_type.type == argument_type_t::type_t::TYPE_NEWID)
+            //     continue;
 
             if (!is_first_arg) ctx.output.write(", ");
             argument.print_type(ctx);
             ctx.output.put(' ');
-            if (!argument.interface.empty()) ctx.output.write("& ");
+            if (!argument.interface.empty() || argument.base_type.type == argument_type_t::type_t::TYPE_NEWID) ctx.output.write("& ");
             argument.print_name(ctx);
 
             is_first_arg = false;
