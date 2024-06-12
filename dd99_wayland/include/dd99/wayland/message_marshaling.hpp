e0f481078914c8cf9e59d2b183404f4e5ba215df
dd99 <delta.deltadev.127@gmail.com>
2024-06-12 12:51:43 -0400

    implemented marshalling for strings (zview) and arrays (span)

--- dd99_wayland/include/dd99/wayland/message_marshaling.hpp
+++ dd99_wayland/include/dd99/wayland/message_marshaling.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include "dd99/wayland/detail/zview.hpp"
+#include <concepts>
 #include <dd99/wayland/engine.hpp>
 
 #include <cstddef>
 #include <cstdint>
 #include <string_view>
-#include <tuple>
 #include <type_traits>
 #include <utility>
 #include <span>
@@ -18,11 +19,22 @@ namespace dd99::wayland::detail
     template <class T>
     inline constexpr std::size_t _marshal_size_one(T && v)
     {
-        if constexpr (std::same_as<std::remove_cvref_t<T>, std::string_view>)
+        // string_view not allowed any more
+        static_assert(!std::same_as<std::remove_cvref_t<T>, std::string_view>);
+
+        if constexpr (std::same_as<std::remove_cvref_t<T>, zview>)
         {
+            auto str_size = v.size() + 1; // account for the null terminator
             // 32-bit size, then the payload, padded to 32-bit alignment
-            return v.size() + sizeof(std::uint32_t) + 
-                (sizeof(std::uint32_t) - 1 - ((v.size() - 1) & (sizeof(std::uint32_t) - 1)));
+            return str_size + sizeof(std::uint32_t) + 
+                (sizeof(std::uint32_t) - 1 - ((str_size - 1) & (sizeof(std::uint32_t) - 1)));
+        }
+        else if constexpr (std::same_as<std::remove_cvref_t<T>, std::span<const char>>)
+        {
+            auto array_size = v.size();
+            // 32-bit size, then the payload, padded to 32-bit alignment
+            return array_size + sizeof(std::uint32_t) + 
+                (sizeof(std::uint32_t) - 1 - ((array_size - 1) & (sizeof(std::uint32_t) - 1)));
         }
         else
         {
@@ -33,15 +45,34 @@ namespace dd99::wayland::detail
     template <class T>
     inline constexpr void message_marshal_one(engine & eng, std::span<int> fds, const T & v)
     {
+        constexpr zview zeroes = "\0\0";
         // TODO: array
 
-        if constexpr (std::same_as<std::remove_cvref_t<T>, std::string_view>)
+        if constexpr (std::same_as<std::remove_cvref_t<T>, zview>)
         {
-            auto size = static_cast<std::uint32_t>(v.size());
-            eng.on_output({reinterpret_cast<char *>(&size), sizeof(size)}, {});
-            eng.on_output(v, fds);
+            auto wire_size = _marshal_size_one(v);
+            auto str_size = static_cast<std::uint32_t>(v.size()) + 1; // account for the null terminator
+            auto padding_size = wire_size - sizeof(std::uint32_t) - str_size;
+
+            assert(padding_size < 4);
+
+            eng.on_output({reinterpret_cast<char *>(&str_size), sizeof(str_size)}, {});
+            eng.on_output({v.data(), str_size}, fds);
+            eng.on_output({zeroes.data(), padding_size}, {});
         }
-        else
+        else if constexpr (std::same_as<std::remove_cvref_t<T>, std::span<const char>>)
+        {
+            auto wire_size = _marshal_size_one(v);
+            auto array_size = static_cast<std::uint32_t>(v.size());
+            auto padding_size = wire_size - sizeof(std::uint32_t) - array_size;
+            
+            assert(padding_size < 4);
+
+            eng.on_output({reinterpret_cast<char *>(&array_size), sizeof(array_size)}, {});
+            eng.on_output(v, fds);
+            eng.on_output({zeroes.data(), padding_size}, {});
+        }
+        else // most things used here are already binary compatible and have a size that is a multiple of 32-bit
         {
             eng.on_output({reinterpret_cast<const char *>(&v), sizeof(v)}, fds);
         }
@@ -70,6 +101,8 @@ namespace dd99::wayland::detail
 
         }
 
+        // message_marshal_one(eng, fds, id);
+        // message_marshal_one(eng, {}, (size << 16) | opcode);
         // (message_marshal_one(eng, {}, std::forward<Args>(args)),...);
     }
 
