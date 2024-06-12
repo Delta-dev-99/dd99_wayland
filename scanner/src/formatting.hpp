acd0fbcf93994c34b35fee9c3ad8bc5b62aa4220
dd99 <delta.deltadev.127@gmail.com>
2024-06-12 12:56:37 -0400

    added check for c++ keywords added current_protocol and current_interface context fields

--- scanner/src/formatting.hpp
+++ scanner/src/formatting.hpp
@@ -44,6 +44,16 @@ inline constexpr std::string_view extern_interface_protocol_name(std::string_vie
     else return "unknown_protocol";
 }
 
+inline bool is_keyword(std::string_view str)
+{
+    static const std::set<std::string_view> keywords =
+    {
+        "alignas","alignof","and","and_eq","asm","auto","bitand","bitor","bool","break","case","catch","char","char8_t","char16_t","char32_t","class","compl","concept","const","consteval","constexpr","constinit","const_cast","continue","co_await","co_return","co_yield","decltype","default","delete","do","double","dynamic_cast","else","enum","explicit","export","extern","false","float","for","friend","goto","if","inline","int","long","mutable","namespace","new","noexcept","not","not_eq","nullptr","operator","or","or_eq","private","protected","public","register","reinterpret_cast","requires","return","short","signed","sizeof","static","static_assert","static_cast","struct","switch","template","this","thread_local","throw","true","try","typedef","typeid","typename","union","unsigned","using","virtual","void","volatile","wchar_t","while","xor","xor_eq",
+    };
+
+    return keywords.contains(str);
+}
+
 
 // used with std::format
 struct indent_lines{
@@ -229,4 +239,6 @@ struct code_generation_context_t
     int indent_size = 4; // TODO: add related option
     int indent_level = 0;
     const std::set<std::string_view> & external_inerface_names;
+    std::string_view current_protocol{};
+    std::string_view current_interface{};
 };
