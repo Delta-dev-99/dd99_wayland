006bfceaf68c7bd1d86e3203d5fc3cff54c4bea7
dd99 <delta.deltadev.127@gmail.com>
2024-06-12 13:02:20 -0400

    added enums added context field (un)setting

--- scanner/src/protocol.hpp
+++ scanner/src/protocol.hpp
@@ -20,36 +20,60 @@ struct protocol_t : element_t
 
     void print_hdr(code_generation_context_t & ctx) const
     {
+        ctx.current_protocol = name;
+
         print_namespace_begin(ctx);
         ctx.indent_level++;
 
+        // fw declaration of interfaces
         ctx.output.put('\n');
         for (const auto & x : interfaces) x.print_fw_declaration(ctx);
 
+        // fw declaration of enums
+        ctx.output.put('\n');
+        ctx.output.format("{}namespace detail {{ // fw declaration of enums\n", whitespace{ctx.indent_size * ctx.indent_level});
+        for (const auto & x : interfaces)
+        for (const auto & y : x.enum_collection)
+        {
+            ctx.output.format(""
+                "{}enum class {}__{} : std::uint32_t;\n"
+            , whitespace{ctx.indent_size * (ctx.indent_level + 1)}
+            , x.name
+            , y.name);
+        }
+        ctx.output.format("{}}}// namespace detail\n", whitespace{ctx.indent_size * ctx.indent_level});
+
+        // interface definitions
         ctx.output.put('\n');
         for (const auto & x : interfaces) x.print_definition(ctx);
 
+        // interface function definitions
         ctx.output.put('\n');
         for (const auto & x : interfaces)
         {
             ctx.output.put('\n');
-            x.print_function_definitions(ctx);
+            x.print_member_definitions_section(ctx);
         }
 
         ctx.indent_level--;
         print_namespace_end(ctx);
+
+        ctx.current_protocol = {};
     }
 
     void print_src(code_generation_context_t & ctx) const
     {
+        ctx.current_protocol = name;
+
         print_namespace_begin(ctx);
         ctx.indent_level++;
 
-        ctx.output.put('\n');
         for (const auto & x : interfaces) x.print_src(ctx);
 
         ctx.indent_level--;
         print_namespace_end(ctx);
+
+        ctx.current_protocol = {};
     }
 
 private:
