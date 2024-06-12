3479362476a539741c51a5a66c02c30936ff5ce2
dd99 <delta.deltadev.127@gmail.com>
2024-06-12 12:48:10 -0400

    added more configurations

--- dd99_wayland/include/dd99/wayland/config.hpp
+++ dd99_wayland/include/dd99/wayland/config.hpp
@@ -15,16 +15,45 @@
     #define DD99_WAYLAND_DEPRECATED
 #endif
 
+
 #if defined(__cplusplus)
 namespace dd99::wayland
 {
+
     consteval auto is_debug_enabled()
     {
-#if not defined(NDEBUG) or defined(DD99_WAYLAND_DEBUG)
+#if defined(DD99_WAYLAND_NO_DEBUG)
+        return false
+#elif not defined(NDEBUG) or defined(DD99_WAYLAND_DEBUG)
         return true;
 #else
         return false;
 #endif
     }
+
+
+    consteval auto is_version_check_enabled()
+    {
+#if defined(DD99_WAYLAND_NO_CHECK_VERSION)
+        return false;
+#elif defined(DD99_WAYLAND_CHECK_VERSION)
+        return true;
+#else
+        return is_debug_enabled();
+#endif
+    }
+
+
+    consteval auto is_wire_debug_enabled()
+    {
+#if defined(DD99_WAYLAND_NO_WIRE_DEBUG)
+        return false
+#elif defined(DD99_WAYLAND_WIRE_DEBUG)
+        return true;
+#else
+        return is_debug_enabled();
+#endif
+    }
+
 }
 #endif
