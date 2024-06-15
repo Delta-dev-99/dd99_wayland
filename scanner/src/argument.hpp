b51fb3c26feb241e87de86c8a5fd217ce6f763c5
dd99 <delta.deltadev.127@gmail.com>
2024-06-15 06:23:35 -0400

    `argument` now inherits from `argument_type` added manual argument creation (constructor) improved syntax for argument type comparison added is_xxx function family to standardise type category checking

--- scanner/src/argument.hpp
+++ scanner/src/argument.hpp
@@ -20,51 +20,40 @@ struct argument_type_t
         "enum",
     };
 
-    enum class type_t {
-        TYPE_INT,
-        TYPE_UINT,
-        TYPE_FIXED,
-        TYPE_OBJECT,
-        TYPE_NEWID,
-        TYPE_STRING,
-        TYPE_ARRAY,
-        TYPE_FD,
-        TYPE_ENUM,
-        TYPE_UNKNOWN, // not actually a type
-    } type{type_t::TYPE_UNKNOWN}; // NOTE: this is the only data member
-
-    static constexpr type_t TYPE_INT = type_t::TYPE_INT;
-    static constexpr type_t TYPE_UINT = type_t::TYPE_UINT;
-    static constexpr type_t TYPE_FIXED = type_t::TYPE_FIXED;
-    static constexpr type_t TYPE_OBJECT = type_t::TYPE_OBJECT;
-    static constexpr type_t TYPE_NEWID = type_t::TYPE_NEWID;
-    static constexpr type_t TYPE_STRING = type_t::TYPE_STRING;
-    static constexpr type_t TYPE_ARRAY = type_t::TYPE_ARRAY;
-    static constexpr type_t TYPE_FD = type_t::TYPE_FD;
-    static constexpr type_t TYPE_ENUM = type_t::TYPE_ENUM;
-    static constexpr type_t TYPE_UNKNOWN = type_t::TYPE_UNKNOWN;
+    enum underlying_type_t {
+        T_INT,
+        T_UINT,
+        T_FIXED,
+        T_OBJECT,
+        T_NEWID,
+        T_STRING,
+        T_ARRAY,
+        T_FD,
+        T_ENUM,
+        T_UNKNOWN, // not actually a type
+    };
 
 
-    static constexpr std::string_view protocol_basic_type_to_cpp_type_string(type_t type)
+    static constexpr std::string_view protocol_basic_type_to_cpp_type_string(underlying_type_t type)
     {
         {
             switch (type) {
-                case type_t::TYPE_INT:     return "std::int32_t";
-                case type_t::TYPE_UINT:    return "std::uint32_t";
-                case type_t::TYPE_FIXED:   return "dd99::wayland::proto::fixed_point";
-                case type_t::TYPE_STRING:  return "zview";
-                case type_t::TYPE_ARRAY:   return "std::span<const char>";
-                case type_t::TYPE_FD:      return "int";
+                case underlying_type_t::T_INT:     return "std::int32_t";
+                case underlying_type_t::T_UINT:    return "std::uint32_t";
+                case underlying_type_t::T_FIXED:   return "dd99::wayland::proto::fixed_point";
+                case underlying_type_t::T_STRING:  return "zview";
+                case underlying_type_t::T_ARRAY:   return "std::span<const char>";
+                case underlying_type_t::T_FD:      return "int";
                 // case type_t::TYPE_ENUM:    return "std::int32_t";
                 default: return "SCANNER__UNKNOWN_TYPE";
             }
         }
     }
-
-    constexpr std::string_view protocol_basic_type_to_cpp_type_string() const
-    { return protocol_basic_type_to_cpp_type_string(type); }
-
     
+
+    constexpr argument_type_t(underlying_type_t v) noexcept
+        : m_type{v}
+    { }
 
     explicit argument_type_t(pugi::xml_node node)
     {
@@ -72,19 +61,27 @@ struct argument_type_t
         {
             if (node.attribute("type").value() == protocol_type_string[i])
             {
-                type = type_t{i};
+                m_type = static_cast<underlying_type_t>(i);
                 break;
             }
         }
     }
 
-    constexpr bool operator==(type_t v) const noexcept { return type == v; }
+
+    constexpr std::string_view protocol_basic_type_to_cpp_type_string() const
+    { return protocol_basic_type_to_cpp_type_string(m_type); }
+
+    constexpr bool operator==(argument_type_t v) const noexcept { return m_type == v.m_type; }
+    // constexpr bool operator==(underlying_type_t v) const noexcept { return m_type == v; }
+
+
+    underlying_type_t m_type{T_UNKNOWN}; // NOTE: this is the only data member
 };
 
 
-struct argument_t : element_t
+struct argument_t : element_t, private argument_type_t
 {
-    argument_type_t base_type;
+    // argument_type_t base_type;
     std::string_view interface;
     std::string_view enum_interface{};
     std::string_view enum_name{};
@@ -92,7 +89,7 @@ struct argument_t : element_t
 
     argument_t(pugi::xml_node node)
         : element_t{node}
-        , base_type{node}
+        , argument_type_t{node}
         , interface{node.attribute("interface").value()}
         , allow_null{node.attribute("allow-null").as_bool()}
     {
@@ -102,22 +99,48 @@ struct argument_t : element_t
         enum_name = enum_sv.substr(dot_position + 1);
     }
 
+    argument_t(element_t && base_element_
+             , argument_type_t base_type_
+             , bool allow_null_ = false
+             , std::string_view interface_ = {}
+             , std::string_view enum_interface_ = {}
+             , std::string_view enum_name_ = {})
+        : element_t{std::move(base_element_)}
+        , argument_type_t{base_type_}
+        , interface{interface_}
+        , enum_interface{enum_interface_}
+        , enum_name{enum_name_}
+        , allow_null{allow_null_}
+    { }
+
+    // get type
+    constexpr argument_type_t & type() noexcept { return *this; }
+    constexpr const argument_type_t & type() const noexcept { return *this; }
+
+    constexpr bool is_fd() const noexcept { return type() == T_FD; }
+    constexpr bool is_array() const noexcept { return type() == T_ARRAY; }
+    constexpr bool is_string() const noexcept { return type() == T_STRING; }
+    constexpr bool is_enum() const noexcept { return type() == T_ENUM || !enum_name.empty(); } // some enums use 'T_UINT' type tag
+    constexpr bool is_new_interface() const noexcept { return type() == T_NEWID; }
+    constexpr bool is_unspecified_new_interface() const noexcept { return is_new_interface() && interface.empty(); }
+    constexpr bool is_existent_interface() const noexcept { return type() == T_OBJECT; }
+    constexpr bool is_interface() const noexcept { return is_new_interface() || is_existent_interface(); }
+
+    constexpr bool can_ommit_type_in_log() const noexcept { return type() == T_STRING || type() == T_INT || type() == T_UINT || type() == T_FIXED; }
+
 
     void print_type(code_generation_context_t & ctx) const
     {
-        auto is_enum = [&](){ return base_type.type == argument_type_t::type_t::TYPE_ENUM || !enum_name.empty(); };
-        auto is_interface = [&](){ return base_type.type == argument_type_t::type_t::TYPE_NEWID || base_type.type == argument_type_t::type_t::TYPE_OBJECT; };
-
-
         if (is_enum())
         {
             auto enum_interface_ = remove_wayland_prefix(enum_interface);
             if (enum_interface_.empty()) enum_interface_ = ctx.current_interface;
 
-            ctx.output.write("detail::");
-            ctx.output.write(enum_interface_);
-            ctx.output.write("__");
-            ctx.output.write(enum_name);
+            ctx.output.format("detail::{}__{}", enum_interface_, enum_name);
+            // ctx.output.write("detail::");
+            // ctx.output.write(enum_interface_);
+            // ctx.output.write("__");
+            // ctx.output.write(enum_name);
         }
         else if (is_interface())
         {
@@ -125,7 +148,7 @@ struct argument_t : element_t
             else if (ctx.external_inerface_names.contains(interface)) ctx.output.format("{}::{}", extern_interface_protocol_name(interface), remove_wayland_prefix(interface));
             else ctx.output.write(remove_wayland_prefix(interface));
         }
-        else ctx.output.write(base_type.protocol_basic_type_to_cpp_type_string());
+        else ctx.output.write(type().protocol_basic_type_to_cpp_type_string());
 
         // auto is_reference = (base_type.type == argument_type_t::type_t::TYPE_OBJECT);
         // if (is_reference) ctx.output.write(" &");
@@ -135,7 +158,7 @@ struct argument_t : element_t
     {
         ctx.output.write(name);
 
-        if (base_type == argument_type_t::TYPE_OBJECT ||
+        if (type() == T_OBJECT ||
             remove_wayland_prefix(interface) == name)
             ctx.output.put('_');
     }
@@ -165,17 +188,14 @@ template <> struct std::formatter<format::argument_type_cpp> : std::formatter<st
     {
         const auto & arg = v.arg;
 
-        bool is_enum = arg.base_type == argument_type_t::TYPE_ENUM || !arg.enum_name.empty();
-        bool is_interface = arg.base_type == argument_type_t::TYPE_NEWID || arg.base_type == argument_type_t::TYPE_OBJECT;
-
-        if (is_enum)
+        if (arg.is_enum())
         {
             auto enum_interface = remove_wayland_prefix(arg.enum_interface);
             if (enum_interface.empty()) enum_interface = v.ctx.current_interface;
 
             std::format_to(ctx.out(), "detail::{}__{}", enum_interface, arg.enum_name);
         }
-        else if (is_interface)
+        else if (arg.is_interface())
         {
             auto interface = remove_wayland_prefix(arg.interface);
             if (interface.empty()) interface = "interface";
@@ -184,7 +204,7 @@ template <> struct std::formatter<format::argument_type_cpp> : std::formatter<st
                 std::format_to(ctx.out(), "{}::{}", extern_interface_protocol_name(arg.interface), interface);
             else std::format_to(ctx.out(), "{}", interface);
         }
-        else std::format_to(ctx.out(), "{}", arg.base_type.protocol_basic_type_to_cpp_type_string());
+        else std::format_to(ctx.out(), "{}", arg.type().protocol_basic_type_to_cpp_type_string());
 
         return ctx.out();
     }
@@ -195,7 +215,7 @@ template <> struct std::formatter<format::argument_name_cpp> : std::formatter<st
     {
         const auto & arg = v.arg;
 
-        bool name_collides = arg.base_type == argument_type_t::TYPE_OBJECT || remove_wayland_prefix(arg.interface) == arg.name;
+        bool name_collides = arg.type() == argument_type_t::T_OBJECT || remove_wayland_prefix(arg.interface) == arg.name;
 
         std::format_to(ctx.out(), "{}{}", arg.name, name_collides ? "_" : "");
 
